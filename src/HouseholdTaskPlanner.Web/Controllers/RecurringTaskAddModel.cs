cef3adf5ceabbe01ce38264e12b57dd233bd71f6
Patrick Krämer <patagonahn@gmail.com>
2020-09-03 00:25:31 +0200

    Add api call to add new recurring tasks

---
+++ src/HouseholdTaskPlanner.Web/Controllers/RecurringTaskAddModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseholdTaskPlanner.Web.Controllers
+{
+    public class RecurringTaskAddModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int IntervalDays { get; set; }
+    }
+}
---
+++ src/HouseholdTaskPlanner.Web/Controllers/RecurringTaskController.cs
@@ -0,0 +1,42 @@
+﻿using HouseholdTaskPlanner.Common.Db;
+using HouseholdTaskPlanner.Common.Db.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HouseholdTaskPlanner.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecurringTaskController : ControllerBase
+    {
+        private readonly IRecurringTaskRepository _recurringTaskRepository;
+        private readonly ScheduledTaskService _scheduledTaskService;
+
+        public RecurringTaskController(IRecurringTaskRepository recurringTaskRepository, ScheduledTaskService scheduledTaskService)
+        {
+            _recurringTaskRepository = recurringTaskRepository;
+            _scheduledTaskService = scheduledTaskService;
+        }
+
+        [HttpGet]
+        public async Task<IList<RecurringTask>> GetList()
+        {
+            return await _recurringTaskRepository.GetAll();
+        }
+
+        [HttpPut]
+        public async Task Put([FromBody] RecurringTaskAddModel addModel)
+        {
+            var newTask = new RecurringTask
+            {
+                Name = addModel.Name,
+                Description = addModel.Description,
+                IntervalDays = addModel.IntervalDays
+            };
+
+            await _recurringTaskRepository.Insert(newTask);
+            await _scheduledTaskService.InitializeRecurringTasks();
+        }
+    }
+}
